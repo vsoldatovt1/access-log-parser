38a6516799daeddc0c76376bc0430f466e511054
Veniamin Soldatov <vsoldatovt1@gmail.com>
2024-09-03 21:28:22 +0300

    Add function for calculating inputed numbers

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+        System.out.println("Сумма чисел = " + (firstNumber + secondNumber) +
+                System.lineSeparator() + "Разность чисел = " + (firstNumber - secondNumber) +
+                System.lineSeparator() + "Произведение чисел = " + (firstNumber * secondNumber) +
+                System.lineSeparator() + "Разность чисел = " + ((double) firstNumber / secondNumber));
     }
 }
