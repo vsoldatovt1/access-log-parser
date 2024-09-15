6982e3e73b603919e109677bd457ddacb7b5d8ba
Veniamin Soldatov <vsoldatovt1@gmail.com>
2024-09-15 21:19:44 +0300

    Course project about cycles

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int rightnum = 0;
+        String path;
+        File file;
+        boolean fileExists;
+        boolean isDirectory;
+        while (true) {
+            System.out.println("Введите путь к существующему файлу.");
+            path = new Scanner(System.in).nextLine();
+            file = new File(path);
+            fileExists = file.exists();
+            isDirectory = file.isDirectory();
+            if (isDirectory) {
+                System.out.println("Вы ввели адрес директории, а не файла.");
+                continue;
+            }
+            if (!fileExists) {
+                System.out.println("Указанный файл по данному пути не существует.");
+                continue;
+            }
+            if (fileExists && !isDirectory) {
+                rightnum++;
+                System.out.println("Путь указан верно. Это файл номер " + rightnum);
+            }
+        }
     }
 }
